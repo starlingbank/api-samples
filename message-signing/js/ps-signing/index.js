3c9997c07a42abd6308af23b5fad2dbf3328d6d7
Saffaanh Soobratty <saffaanh.soobratty@starlingbank.com>
2021-03-17 11:37:51 +0000

    Add put request (#8)
    * remove crypto package
    
    * remove log
    
    * Add put request
    
    Add example put request

--- message-signing/js/ps-signing/index.js
+++ message-signing/js/ps-signing/index.js
@@ -1,25 +1,35 @@
 const axios = require('axios').default;
 const crypto = require('crypto');
 const fs = require('fs');
+const { v4 } = require('uuid');
 
 const baseURL = 'https://payment-api-sandbox.starlingbank.com';
 const privateKeyPath = 'starling-api-private.key';
 const apiKeyUid = 'KEY_UID';
 const paymentBusinessUid = 'PAYMENT_BUSINESS_UID';
 const accountUid = 'ACCOUNT_UID';
-const date = (new Date()).toISOString();
-const url = `/api/v1/${paymentBusinessUid}/account/${accountUid}`;
-const method = 'get';
-const data = ''; // No payload so no digest needed on a GET
+const sortCode = 'SORT_CODE';
 
-const signature = crypto
-    .createSign('RSA-SHA512')
-    .update(`(request-target): get ${url}\nDate: ${date}\nDigest: `)
-    .sign(fs.readFileSync(privateKeyPath), 'base64');
+const calculateAuthorisationAndDigest = (date, method, url, data = '') => {
+    const digest = data === ''
+        ? ''
+        : crypto
+            .createHash('sha512')
+            .update(JSON.stringify(data))
+            .digest('base64');
 
-const authorization = `Signature keyid="${apiKeyUid}",algorithm="rsa-sha512",headers="(request-target) Date Digest",signature="${signature}"`;
+    const signature = crypto
+        .createSign('RSA-SHA512')
+        .update(`(request-target): ${method} ${url}\nDate: ${date}\nDigest: ${digest}`)
+        .sign(fs.readFileSync(privateKeyPath), 'base64');
 
-axios.request({
+    return {
+        digest,
+        authorization: `Signature keyid="${apiKeyUid}",algorithm="rsa-sha512",headers="(request-target) Date Digest",signature="${signature}"`
+    };
+};
+
+const makeRequest = ({ url, method, authorization, date, digest, data = '' }) => axios.request({
     baseURL,
     url,
     method,
@@ -27,10 +37,38 @@ axios.request({
     headers: {
         Authorization: authorization,
         Date: date,
-        Digest: '',
+        Digest: digest,
         'Content-Type': 'application/json',
         'User-Agent': 'api-samples/message-signing/js/ps-signing'
     }
 })
     .then(response => console.log(response.data))
-    .catch(err => console.error(err.response))
+    .catch(err => console.error(err.response));
+
+const getAccount = () => {
+    const date = (new Date()).toISOString();
+    const url = `/api/v1/${paymentBusinessUid}/account/${accountUid}`;
+    const method = 'get';
+
+    const { digest, authorization } = calculateAuthorisationAndDigest(date, method, url);
+
+    return makeRequest({ url, method, authorization, date, digest });
+};
+
+const putAddress = () => {
+    const addressUid = v4();
+    const date = (new Date()).toISOString();
+    const url = `/api/v1/${paymentBusinessUid}/account/${accountUid}/address/${addressUid}`;
+    const method = 'put';
+    const data = {
+        accountName: 'My Account Name',
+        sortCode
+    };
+
+    const { digest, authorization } = calculateAuthorisationAndDigest(date, method, url, data);
+
+    return makeRequest({ url, method, data, authorization, date, digest });
+};
+
+getAccount()
+    .then(() => putAddress());
